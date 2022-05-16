5a2f4fab33fa4f93624b7566c8d99ec8e4a47f8d
Oskar Murand <omurand@twilio.com>
2022-05-16 11:35:05 +0300

    more comments

--- user-service/README.md
+++ user-service/README.md
@@ -9,3 +9,4 @@ When running locally the servcice is available at http://localhost:3001
 - `npm run start:dev` starts the dev service so that the service restarts when the files are modified
 - `npm run start` starts the service
 
+_run these scripts from inside the `user-service` directory_ in the terminal
--- user-service/app.js
+++ user-service/app.js
@@ -1,43 +1,53 @@
 // we import the express module
-const express = require('express')
+const express = require("express");
 
 // we import our users list static JSON
-const usersList = require('./data/users')
+const usersList = require("./data/users");
 
-const app = express()
+const app = express();
 // we define which port the service runs on
-const port = 3001
+const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+/**
+    this defines an API endpoint / which is the root of the application.
+    each endpoint can be defined like this
+    this is a GET endpoint meaning that you can
+    open the browser and navigate to http://localhost:3001/
+    and that would make a request against the path defined here
+*/
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-// when the /users endpoint is queried
-app.get('/users', (req, res) => {
-    /** 
+/*
+    when the /users GET endpoint is queried
+    http://localhost:3001/users
+*/
+app.get("/users", (req, res) => {
+  /** 
         we respond with the whole users list json
         we wrap the data with another object and users key
         we do this so that we can add additonal metadata in the future
         also, tthis allows us to give context to the returned data
     */
-    res.send({ users: usersList })
-})
+  res.send({ users: usersList });
+});
 
 // when the /users/<any string> endpoint is queried
-app.get('/users/:id', (req, res) => {
-    // we search the objects in usersList array for matching __id and assign it to user if found
-    const user = usersList.find(userObject => req.params.id === userObject._id)
-    if (user) {
-        // if user is found we return the user
-        res.send({ user: user })
-    } else {
-        // else we return 404 - not found
-        res.sendStatus(404)
-    }
-})
+app.get("/users/:id", (req, res) => {
+  // we search the objects in usersList array for matching __id and assign it to user if found
+  const user = usersList.find((userObject) => req.params.id === userObject._id);
+  if (user) {
+    // if user is found we return the user
+    res.send({ user: user });
+  } else {
+    // else we return 404 - not found
+    res.sendStatus(404);
+  }
+});
 
-app.get('/active/count', (req, res) => {
-    /** 
+app.get("/active/count", (req, res) => {
+  /** 
         implement the active users endpoint so that it would return the active user count
         returned data example:
             { active-users: 5 }
@@ -45,9 +55,9 @@ app.get('/active/count', (req, res) => {
         in order to filter the usersList .filter() would be helpful
         https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
     */
-})
+});
 
 // the service is started on the configured port when this file is run
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
